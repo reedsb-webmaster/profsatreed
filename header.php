6c86179cf599ea56d62bcecbdde9831a5fc4715a
Lucas Hamilton <lucas@macobserver.com>
2021-10-22 12:45:09 -0700

    Set up and include header

---
+++ header.php
@@ -0,0 +1,18 @@
+<div class="header container">
+  <div class="navbar">
+    <div class="navbar-left">
+      <a class="navbar-brand" href="<?php echo SITE_URL; ?>"><img class="img-responsive" src="https://www.profsatreed.com/img/reed-logo-img.png" alt="<?php echo SITE_TITLE; ?>"></a>
+    </div>
+    <div class="navbar navbar-right">
+      <div class="tab">
+        <h1> Results </h1>
+      </div>
+      <div class="tab">
+        <h1> Advisor Feedback </h1>
+      </div>
+      <div class="tab">
+        <h1> Professor Feedback </h1>
+      </div>
+    </div>
+  </div>
+</div>
