dbf7f1a8862bd3d0a5c87e9721118075d6b5f4f6
T W <ymir@ulthar.xyz>
2021-12-11 19:22:13 -0800

    We'll insert all the content with JavaScript

--- generate.py
+++ generate.py
@@ -8,41 +8,34 @@ from mako.template import Template
 
 t = Template(filename="template.html")
 
-advisor_form = "https://docs.google.com/forms/d/e/1FAIpQLSe750TrU5eKALR8WIZxF_4sKmpKwEnd77-1fNHCvCoMbkCj-Q/viewform?embedded=true"
-
-professor_form = "https://docs.google.com/forms/d/e/1FAIpQLSe750TrU5eKALR8WIZxF_4sKmpKwEnd77-1fNHCvCoMbkCj-Q/viewform?embedded=true"
 
 def home() -> str:
     """
     Assemble the site's home page
     """
     return t.render(title = "Home | Profs at Reed",
-                    pageheader = "Home",
-                    body = "Welcome!")
+                    pageheader = "Home")
 
 def advisor_feedback() -> str:
     """
     Assemble a feedback form for the advisor role.
     """
     return t.render(title = "Advisor Feedback | Profs at Reed",
-                    pageheader = "Advisor Feedback",
-                    body = "<iframe src='" + professor_form + "'>loading...<iframe>")
+                    pageheader = "Advisor Feedback")
 
 def professor_feedback() -> str:
     """
     Assemble a feedback form for the advisor role.
     """
     return t.render(title = "Professor Feedback | Profs at Reed",
-                    pageheader = "Professor Feedback",
-                    body = "<iframe src='" + professor_form + "'>loading...<iframe>")
+                    pageheader = "Professor Feedback")
 
 def results() -> str:
     """
     Assemble a page with a list of the results, requested from the "database".
     """
     return t.render(title = "View Feedback | Profs at Reed",
-                    pageheader = "View Feedback",
-                    body = "You'll be able to view feedback here.")
+                    pageheader = "View Feedback")
 
 def gen_pages():
     with open("index.html", "w") as f:
